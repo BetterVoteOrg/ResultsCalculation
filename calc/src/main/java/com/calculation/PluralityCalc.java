b5e24d46febcca8722e880f7804807bf45af7358
George Adler Buras <gburas4@lsu.edu>
2021-11-05 00:44:29 -0500

    PluralityCalc tie functionality and lots of tests

--- calc/src/main/java/com/calculation/PluralityCalc.java
+++ calc/src/main/java/com/calculation/PluralityCalc.java
@@ -1,43 +1,80 @@
 package com.calculation;
 
+import java.util.ArrayList;
+
 public class PluralityCalc 
 {
+    private static boolean tie = false;
+
     public static int findMaxIndex(int[] array)
     {
-        int max = 0;
+        int maxIndex = 0;
         for (int i=1; i < array.length; i++)
         {
-            if (array[i] > array[max])
-                max = i;
+            if (array[i] > array[maxIndex])
+            {
+                maxIndex = i;
+                tie = false;
+            }
+            else if (array[i] == array[maxIndex])
+                tie = true;
         }
-        return max;
+        return maxIndex;
     }
 
-    public static String[] calculate(String[] choices, int[][] votes) {
+    public static ArrayList<Integer> findTiedWinnerIndexes(int winningVote, int[] tally)
+    {
+        ArrayList<Integer> tiedWinnerIndexes = new ArrayList<Integer>();
+
+        for (int i=0; i < tally.length; i++)
+        {
+            if (tally[i] == winningVote)
+                tiedWinnerIndexes.add(i);
+        }
+
+        return tiedWinnerIndexes;
+    }
+
+    public static String[] calculate(String[] choices, int[][] votes) 
+    {
         String[] outcome = new String[2];
         String winner;
         String results = "";
         int[] tally = new int[choices.length];
+        int winnerIndex;
+        ArrayList<Integer> tiedWinnerIndexes;
 
         for (int i=0; i < votes.length; i++)
         {
             tally[votes[i][0]]++;
         }
 
-        winner = choices[findMaxIndex(tally)];
+        winnerIndex = findMaxIndex(tally);
+
+        if (tie == false)
+            winner = choices[winnerIndex];
+        else
+        {
+            tiedWinnerIndexes = findTiedWinnerIndexes(tally[winnerIndex], tally);
+            winner = "Tie between " + choices[tiedWinnerIndexes.get(0)];
+            for (int i=1; i < tiedWinnerIndexes.size()-1; i++)
+            {
+                winner = winner + ", " + choices[tiedWinnerIndexes.get(i)];
+            }
+            if (tiedWinnerIndexes.size() > 1)
+                winner = winner + " and " + choices[tiedWinnerIndexes.get(tiedWinnerIndexes.size()-1)];
+        }
 
         for (int i=0; i < tally.length; i++)
         {
-            results = results + choices[i] + " got " + tally[i] + " votes.\n";
+            if (tally[i] != 1)
+                results = results + choices[i] + " got " + tally[i] + " votes.\n";
+            else
+                results = results + choices[i] + " got " + tally[i] + " vote.\n";
         }
 
         outcome[0] = winner;
         outcome[1] = results;
         return outcome;
-    }
-
-    public static int returnSeven() 
-    {
-        return 7;
     }
 }
--- calc/src/test/java/com/calculation/PluralityCalcTest.java
+++ calc/src/test/java/com/calculation/PluralityCalcTest.java
@@ -1,14 +1,22 @@
 package com.calculation;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class PluralityCalcTest 
 {
     @Test
-    public void testCalculate()
+    public void testCalculate0()
+    {
+        String[] choices = {"Jim", "Pam"};
+        int[][] votes = {};
+        String[] output = {"Tie between Jim and Pam", "Jim got 0 votes.\nPam got 0 votes.\n"};
+        assertArrayEquals(output, PluralityCalc.calculate(choices, votes));
+    }
+
+    @Test
+    public void testCalculate1()
     {
         String[] choices = {"Jim", "Pam"};
         int[][] votes = {{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}, {1}};
@@ -17,8 +25,38 @@ public class PluralityCalcTest
     }
 
     @Test
-    public void testReturnSeven()
+    public void testCalculate2()
     {
-        assertEquals(7, PluralityCalc.returnSeven());
+        String[] choices = {"Jim", "Pam"};
+        int[][] votes = {{0}, {0}, {1}, {1}};
+        String[] output = {"Tie between Jim and Pam", "Jim got 2 votes.\nPam got 2 votes.\n"};
+        assertArrayEquals(output, PluralityCalc.calculate(choices, votes));
+    }
+
+    @Test
+    public void testCalculate3()
+    {
+        String[] choices = {"Jim", "Pam", "Michael"};
+        int[][] votes = {{0}, {0}, {1}, {1}, {2}};
+        String[] output = {"Tie between Jim and Pam", "Jim got 2 votes.\nPam got 2 votes.\nMichael got 1 vote.\n"};
+        assertArrayEquals(output, PluralityCalc.calculate(choices, votes));
+    }
+
+    @Test
+    public void testCalculate4()
+    {
+        String[] choices = {"Jim", "Pam", "Michael"};
+        int[][] votes = {{0}, {0}, {1}, {1}, {2}, {2}};
+        String[] output = {"Tie between Jim, Pam and Michael", "Jim got 2 votes.\nPam got 2 votes.\nMichael got 2 votes.\n"};
+        assertArrayEquals(output, PluralityCalc.calculate(choices, votes));
+    }
+
+    @Test
+    public void testCalculate5()
+    {
+        String[] choices = {"Jim", "Pam", "Michael"};
+        int[][] votes = {{2}, {0}, {0}, {2}, {1}, {1}, {2}, {0}, {1}, {2}, {0}, {1}, {2}, {2}, {1}, {0}};
+        String[] output = {"Michael", "Jim got 5 votes.\nPam got 5 votes.\nMichael got 6 votes.\n"};
+        assertArrayEquals(output, PluralityCalc.calculate(choices, votes));
     }
 }
