e1b6e9f0391219b6f4ad1f286033fa51e6b105dc
George Adler Buras <gburas4@lsu.edu>
2021-11-25 00:55:04 -0600

    Created bare bones for RCV calculation

---
+++ calc/src/main/java/com/calculation/RcvCalc.java
@@ -0,0 +1,23 @@
+package com.calculation;
+
+//import java.util.ArrayList;
+
+public class RcvCalc {
+    private static String results;
+
+    public static int[] tallyFirstChoices(int[][] votes)
+    {
+        int[] tally = new int[votes[0].length];
+
+        return tally;
+    }
+
+    public static String[] calculate(String[] choices, int[][] votes)
+    {
+        results = "";
+        String[] outcome = new String[2];
+
+        outcome[1] = results;
+        return outcome;
+    }
+}
